1421c6711f2973f9cd26c843598b59dee875b7e6
abplas <abplascencia@csumb.edu>
2025-10-02 13:55:03 -0700

    Added Springboot Set Up files
    Contains the following needed for our project
    Spring Web Web
    Build web, including RESTful, applications using Spring MVC. Uses Apache Tomcat as the default embedded container.
    Spring Data JPA SQL
    Persist data in SQL stores with Java Persistence API using Spring Data and Hibernate.
    Spring Boot DevTools Developer Tools
    Provides fast application restarts, LiveReload, and configurations for enhanced development experience.
    OAuth2 Resource Server Security
    Spring Boot integration for Spring Security's OAuth2 resource server features.

---
+++ src/main/java/com/example/demo/Cst438P2Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cst438P2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cst438P2Application.class, args);
+	}
+
+}
